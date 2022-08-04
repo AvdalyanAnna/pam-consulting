06667d90cf0e14a7cd7aa8c7dfe30d82d6d99f66
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-08-04 15:26:00 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+let swiper = new Swiper(".questions__list", {
+	slidesPerView: 1,
+	spaceBetween: 20,
+	pagination: {
+
+		el: ".swiper-pagination",
+	},
+	breakpoints: {
+		600: {
+			slidesPerView: 2
+		},
+		1200: {
+			slidesPerView: 3
+		},
+	}
+});
+let swiper1 = new Swiper(".first__list", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+	}
+});
+document.querySelectorAll('.menu').forEach(btn => {
+	btn.addEventListener('click', e => {
+		btn.classList.toggle('active');
+		document.querySelector('.mobile-menu').classList.toggle('active');
+	});
+});
