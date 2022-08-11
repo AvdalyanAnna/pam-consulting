5021c45f1f9f74c01525775920053b78d744018b
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-08-11 16:23:48 +0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,39 @@
 let swiper = new Swiper(".questions__list", {
-	slidesPerView: 1,
-	spaceBetween: 20,
-	pagination: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
 
-		el: ".swiper-pagination",
-	},
-	breakpoints: {
-		600: {
-			slidesPerView: 2
-		},
-		1146: {
-			slidesPerView: 3
-		},
-	}
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1146: {
+            slidesPerView: 3
+        },
+    }
 });
 let swiper1 = new Swiper(".first__list", {
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-	}
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    }
 });
 document.querySelectorAll('.menu').forEach(btn => {
-	btn.addEventListener('click', e => {
-		btn.classList.toggle('active');
-		document.querySelector('.mobile-menu').classList.toggle('active');
-	});
+    btn.addEventListener('click', e => {
+        btn.classList.toggle('active');
+        document.querySelector('.mobile-menu').classList.toggle('active');
+    });
 });
+$(document).ready(function () {
+    $('.img-map__item').on('click', function () {
+        const show = $(this).data('show')
+        $('.map .map-item').slideUp()
+        $(show).slideDown()
+    })
+
+})
